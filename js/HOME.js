cef59245c7a37acc70b89434f13cf8573e45369e
Moustafa-M-Nasr <42911456+moustafanasr@users.noreply.github.com>
2024-02-01 23:34:06 +0200

    created by moustafa nasr

---
+++ js/HOME.js
@@ -0,0 +1,7 @@
+const home = document.getElementById("HOME");
+const reve = document.getElementById("con-remove")
+
+function tohome() {
+  home.classList.add('Active');
+  reve.style.display= 'none';
+}
